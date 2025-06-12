b578011815a4275701a6957faa746e42350aaae5
Daniho100 <daniandre.dah@gmail.com>
2025-06-12 15:57:13 +0100

    hosting backend cors

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -52,7 +52,7 @@ const loginUser = async(req, res, next) => {
     }
 
     const payload = { userId: user._id };
-    const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h' });
+    const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1d' });
 
     res.status(200).json({
       message: 'Login successful',
